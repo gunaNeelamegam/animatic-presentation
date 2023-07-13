c4e5c140c4dd3ea27e7eeaf2fcd595adf247448b
guna <guna.n@zilogic.com>
2023-07-13 12:46:57 +0530

    Added: audio and video tags alternative and section tags .

--- build.py
+++ build.py
@@ -9,9 +9,10 @@ command to build html page.
 
 from subprocess import run
 import os
-from bs4 import BeautifulSoup
-import os
 import base64
+from bs4 import BeautifulSoup
+from requests import get
+from inquirer import Text, List, prompt, themes
 
 SOURCE_FILE_NAME = ""
 
@@ -25,6 +26,98 @@ class RevealJsException(Exception):
     pass
 
 
+"""
+Creating the Project template folder.
+"""
+INITIAL_MESSAGE = "creating the revealjs presentation \n NOTE:\n \t* audio and video content if project needs create either audio and video \n\n \t\t(or)\n\n\t\t figures may contains all the resources file's.... \n\n\n"
+
+
+def get_audiodata(url: str):
+    sub_type = url.split(".")[-1]
+    content = get_base64_data(url)
+    if sub_type in audio_formats:
+        print("Audio File Present", sub_type, url)
+        return f"data:audio/{sub_type};base64,{content}"
+
+
+def get_videodata(url: str):
+    sub_type = url.split(".")[-1]
+    content = get_base64_data(url)
+    if sub_type in video_formats:
+        print("Video File Present", sub_type, url)
+        return f"data:video/{sub_type};base64,{content}"
+
+
+def create_template():
+    prompt_list = [
+        Text(
+            "slide_name",
+            "project name for reveal js presentation \n example: slide.adoc",
+            default="slide.adoc",
+            show_default="slide.adoc",
+        ),
+        List(
+            "audio",
+            "create the audio directory for audio files ",
+            choices=["yes", "no"],
+            default="no",
+        ),
+        List(
+            "video",
+            message="create the video directory for video files",
+            choices=["yes", "no"],
+            default="no",
+        ),
+        List(
+            "figures",
+            message="create a figures directory for all the images , video ,audio file's",
+            default="yes",
+            choices=["yes", "no"],
+        ),
+    ]
+    answers = prompt(
+        prompt_list,
+        theme=themes.BlueComposure(),
+    )
+    for question in answers.keys():
+        print(question)
+        if question == "slide_name":
+            if (
+                len(
+                    list(
+                        filter(
+                            lambda file_ext: file_ext in str(answers.get(question)),
+                            [".md", ".adoc", ".asciidoc"],
+                        )
+                    )
+                )
+                >= 1
+            ):
+                create_file(file_path=answers.get(question))
+        else:
+            for key, value in answers.items():
+                if value == "yes":
+                    os.makedirs(key, exist_ok=True)
+
+            # print(
+            #     *map(
+            #         lambda args: (
+            #             not os.path.exists(args.get("yes")) and create_dir(**args)
+            #         ),
+            #         filter(
+            #             None,
+            #             map(
+            #                 lambda key: {answers.get(key): key}
+            #                 if answers.get(key) == "yes"
+            #                 else None,
+            #                 answers.keys(),
+            #             ),
+            #         ),
+            #     )
+            # )
+            return
+
+
 def install_script(command):
     return run(command, shell=True, stderr=open(os.devnull))
 
@@ -33,7 +126,14 @@ def install_deps():
     """
     Installation for building the reveal js presentation.
     """
-    map(install_script, DEPS)
+    if not os.path.exists("node_modules"):
+        [*map(install_script, DEPS)]
+    else:
+        return
+
+
+video_formats = ["mp4", "avi", "mkv", "mov", "wmv"]
+audio_formats = ["mp3", "wav", "aac", "ogg", "flac"]
 
 
 def run_npx_with_asciidoc(source_filename="slides.adoc"):
@@ -57,12 +157,10 @@ def run_npx_with_asciidoc(source_filename="slides.adoc"):
         raise RevealJsException("FILE NAME IS NOT PRESEN ...")
 
 
-from requests import get
-
-
-def get_imagedata(url):
-    if (("http") or ("https")) in url:
+def get_imagedata(url: str):
+    if url.__contains__("http") or url.__contains__("https"):
         data = get(url, stream=True)
+        print(data)
         return base64.b64encode(data.content).decode()
     else:
         return get_base64_data(url)
@@ -75,6 +173,9 @@ def extract_paths(html_file):
         link_tags = soup.find_all("link")
         img_tag = soup.find_all("img")
         section_tag = soup.find_all("section")
+        audio_tags = soup.find_all("audio")
+        video_tags = soup.find_all("video")
+
         paths = []
 
         # Extract paths from <script> tags
@@ -111,17 +212,78 @@ def extract_paths(html_file):
                     img.replace_with(empty_img_tag)
 
         for section in section_tag:
+            video_path = ""
+            image_path = ""
             if "data-background-image" in section.attrs:
-                path = section.get("data-background-image")
-                print(path)
-                image_type = path.strip().split(".")[-1]
-                img_data = get_imagedata(path)
-                empty_section_tag = soup.new_tag("section")
-                empty_section_tag.attrs = {
-                    **section.attrs,
-                    "data-background-image": f"data:image/{image_type};base64, {img_data}",
-                }
-                section.replace_with(empty_section_tag)
+                image_path = section.get("data-background-image")
+                if image_path:
+                    image_type = image_path.strip().split(".")[-1]
+                    img_data = get_imagedata(image_path)
+                    empty_section_tag = soup.new_tag("section")
+                    empty_section_tag.attrs = {
+                        **section.attrs,
+                        "data-background-image": f"data:image/{image_type};base64,{img_data}",
+                    }
+                    section.replace_with(empty_section_tag)
+                    return
+            else:
+                video_path = section.get("data-background-video")
+                if video_path and not (
+                    "http" in str(video_path) or "https" in str(video_path)
+                ):
+                    video_type = video_path.strip().split(".")[-1]
+                    img_data = get_imagedata(video_path)
+                    empty_section_tag = soup.new_tag("section")
+                    empty_section_tag.attrs = {
+                        **section.attrs,
+                        "data-background-video": f"data:video/{video_type};base64,{img_data}",
+                    }
+                    section.replace_with(empty_section_tag)
+
+        for audio in audio_tags:
+            try:
+                if "src" in audio.attrs:
+                    path = audio.get("src")
+                    if ("audio" or "music" or "figures") in path:
+                        empty_audio_tag = soup.new_tag("audio")
+                        empty_audio_tag.attrs = {
+                            **audio.attrs,
+                            "src": get_audiodata(path),
+                        }
+
+                        audio.replace_with(empty_audio_tag)
+
+            except Exception as e:
+                print(
+                    f"""
+                            {'*'*30}
+                            \t \t
+                            {e.args}
+                            \t \t
+                            {'*'*30}
+                    """
+                )
+            for video in video_tags:
+                try:
+                    if "src" in video.attrs:
+                        path = video.get("src")
+                        if ("video" or "figures" or "music") in path:
+                            empty_video_tag = soup.new_tag("video")
+                            empty_video_tag.attrs = {
+                                **video.attrs,
+                                "src": get_videodata(path),
+                            }
+
+                            video.replace_with(empty_audio_tag)
+
+                except Exception as e:
+                    print(
+                        f"""
+                                {'*'*30}
+                                {e.args}
+                                {'*'*30}
+                        """
+                    )
 
         modified_html = str(soup)
         delete_file(html_file)
@@ -137,7 +299,7 @@ def delete_file(filename):
     os.remove(filename)
 
 
-def create_file(content, file_path):
+def create_file(content="", file_path=""):
     with open(file_path, "w") as modified_file:
         modified_file.write(content)
 
@@ -181,5 +343,7 @@ if __name__ == "__main__":
     creating the index.html file from your asciidoc file
     """
     for file in get_rstfilename():
+        print(INITIAL_MESSAGE)
+        # create_template()
         print(file)
         build(file)
